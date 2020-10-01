ec137fc321cf847204f50465d822769d0b2a16fe
Shantovai23 <hasibulhasan23sept@gmail.com>
2020-10-02 01:23:32 +0600

    Updates

---
+++ precache-manifest.855bb330c19d797809e96595f16872d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba374f95dce0b1f9dcf4387dc3c6085",
+    "url": "/Covid19-app-Reactjs/index.html"
+  },
+  {
+    "revision": "d17ffdab1f5a634ffc28",
+    "url": "/Covid19-app-Reactjs/static/css/main.0ca6b6e9.chunk.css"
+  },
+  {
+    "revision": "1aacacf4e591c3a572a6",
+    "url": "/Covid19-app-Reactjs/static/js/2.18674b8f.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/Covid19-app-Reactjs/static/js/2.18674b8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d17ffdab1f5a634ffc28",
+    "url": "/Covid19-app-Reactjs/static/js/main.a99903e3.chunk.js"
+  },
+  {
+    "revision": "13e66c88c6cb27e64d1c",
+    "url": "/Covid19-app-Reactjs/static/js/runtime-main.7afe3ab4.js"
+  }
+]);
